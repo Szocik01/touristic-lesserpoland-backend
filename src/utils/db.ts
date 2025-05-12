01e23659046cd333fd8b36ccddf9da4a9ab31859
Wiktor <wiktor01@op.pl>
2025-05-12 21:44:07 +0200

    Fix backend connection and constants

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,11 +1,11 @@
 import { Pool } from "pg";
 
 export default new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "touristic_lesserpoland",
-  password: "root",
-  port: 5432,
+user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT),
   max: 20,
   idleTimeoutMillis: 30000,
 });
